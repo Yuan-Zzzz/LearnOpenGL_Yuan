9537b166235bfd640e07d9cc1b0864f1b3d00eae
Henry_Zhang <1729726526@qq.com>
2024-08-16 14:31:25 +0800

    hello_window

--- src/01_hello_window/main.cpp
+++ src/01_hello_window/main.cpp
@@ -1,7 +1,66 @@
-#include<GLAD/glad.h>
-#include<GLFW/glfw3.h>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+// 窗口大小改变时的回调函数，窗口大小改变时，改变视口大小
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
-int main(){
+int main()
+{
+    // 初始化glfw
+    glfwInit();
+    // 配置glfw
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-return 0;
+    // 创建窗口
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // 告诉OpenGL视口大小（渲染窗口的尺寸）
+    glViewport(0, 0, 800, 600);
+
+    // 注册回调
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        // 监听输入
+        processInput(window);
+
+        // 渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 释放资源
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
