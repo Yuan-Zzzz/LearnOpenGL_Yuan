c11fe4c1d9eed21f4729f9288b58a7deb1f32c73
Henry_Zhang <1729726526@qq.com>
2024-08-22 22:59:51 +0800

    理解代码，添加注释

--- src/04_textures/main.cpp
+++ src/04_textures/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 
   // 顶点数据
   float vertices[] = {
-      //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+      //---- 位置 -------- 颜色 -----纹理坐标-
       0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // 右上
       0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // 右下
       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0, 0.0f, // 左下
@@ -66,15 +66,15 @@ int main(int argc, char *argv[])
 
   // 绑定VAO缓冲对象
   glBindVertexArray(VAO);
-
-  // 绑定VBO缓对象
+  // 绑定VBO缓冲对象
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  // 填充VBO数据
+  // 填充VBO数据（将顶点数据赋值到VBO中）
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+  //绑定EBO缓冲对象
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  /*-------------------------------------告诉VAO如何解析VBO的数据---------------------------------------*/
   // 设置顶点位置属性指针
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -86,7 +86,9 @@ int main(int argc, char *argv[])
   // 设置顶点纹理坐标属性指针
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
+  /*-------------------------------------告诉VAO如何解析VBO的数据---------------------------------------*/
 
+  //解绑VAO
   glBindVertexArray(0);
 
   // 生成纹理
@@ -103,7 +105,7 @@ int main(int argc, char *argv[])
   // 图像y轴翻转
   stbi_set_flip_vertically_on_load(true);
 
-  // 加载图片
+  // 加载图片1
   int width, height, nrChannels;
   unsigned char *data = stbi_load("./static/texture/container.jpg", &width, &height, &nrChannels, 0);
 
@@ -123,7 +125,7 @@ int main(int argc, char *argv[])
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  // 加载图片
+  // 加载图片2
   data = stbi_load("./static/texture/awesomeface.png", &width, &height, &nrChannels, 0);
 
   if (data)
@@ -132,6 +134,7 @@ int main(int argc, char *argv[])
     glGenerateMipmap(GL_TEXTURE_2D);
   }
   stbi_image_free(data);
+
   ourShader.use();
   ourShader.setInt("texture1", 0);
   ourShader.setInt("texture2", 1);
@@ -140,20 +143,21 @@ int main(int argc, char *argv[])
   {
     processInput(window);
 
-    // 渲染指令
-    // ...
     glClearColor(25.0 / 255.0, 25.0 / 255.0, 25.0 / 255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
     ourShader.use();
 
+    //激活纹理单元
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture1);
 
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
+    //绑定VAO
     glBindVertexArray(VAO);
+    //绘制绑定得VAO中的元素
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
--- src/04_textures/shader.frag
+++ src/04_textures/shader.frag
@@ -7,6 +7,6 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
-  // FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
-  FragColor = texture(texture1, TexCoord);
+  FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+  //FragColor = texture(texture1, TexCoord);
 }
